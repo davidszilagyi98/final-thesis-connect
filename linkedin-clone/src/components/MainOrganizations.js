5d6530aaa47655b93c28dd324074d185bdf84f24
@davidszilagyi98 <eaadsz@students.eaaa.dk>
2022-05-06 15:48:13 +0200

    organisation cards in grid

--- linkedin-clone/src/components/MainOrganizations.js
+++ linkedin-clone/src/components/MainOrganizations.js
@@ -4,24 +4,20 @@ import { db } from "../firebase";
 import { collection, onSnapshot } from "@firebase/firestore";
 import { Link } from "react-router-dom";
 
-
 const MainOrganizations = ({ name }) => {
-
   const [users, setUsers] = useState([]);
   useEffect(() => onSnapshot(collection(db, "users"), (snapshot) => setUsers(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))), []);
 
-  
   return (
     <Container>
       <OrganizationsList>
         <ul>
-          <h2>Titel</h2>
           {users.map((user) => (
             <Link to={`/userprofile/${user.id}`}>
-            <li key={user.id}>
+              <li key={user.id}>
                 <p>{user.name}</p>
-            </li>
-              </Link>
+              </li>
+            </Link>
           ))}
         </ul>
       </OrganizationsList>
@@ -34,29 +30,37 @@ const Container = styled.div`
 `;
 
 const OrganizationsList = styled.div`
-    display: block;
+  ul {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    margin-top: 2rem;
 
-    li {
-        list-style-type: none;
-        background-color: #fff;
-        background: linear-gradient(to left, white 97%, #063A54 3%);
-        box-shadow: 0px 3px 6px 2px rgba(207, 207, 207, 0.2);
-        -webkit-box-shadow: 0px 3px 6px 2px rgba(207, 207, 207, 0.2);
-        -moz-box-shadow: 0px 3px 6px 2px rgba(207, 207, 207, 0.2);
-        border-radius: 12px;
-        display: flex;
-        width: 40%;
-        text-align: left;
-        margin-bottom: 10px;
-        padding: 30px;
-
-  .namewithflag {
-    display: flex;
-    flex-direction: row;
-
-    a {
-      margin-right: 3px;
+    @media (max-width: 768px) {
+      margin: 20px auto;
+      grid-template-columns: 1fr;
     }
+  }
+  li {
+    list-style-type: none;
+    background-color: #fff;
+    background: linear-gradient(to left, white 97%, #063a54 3%);
+    box-shadow: 0px 3px 6px 2px rgba(207, 207, 207, 0.2);
+    -webkit-box-shadow: 0px 3px 6px 2px rgba(207, 207, 207, 0.2);
+    -moz-box-shadow: 0px 3px 6px 2px rgba(207, 207, 207, 0.2);
+    border-radius: 12px;
+    display: flex;
+    text-align: left;
+    margin-bottom: 10px;
+    margin-right: 1rem;
+    padding: 30px;
+
+    .namewithflag {
+      display: flex;
+      flex-direction: row;
+
+      a {
+        margin-right: 3px;
+      }
     }
   }
 `;
